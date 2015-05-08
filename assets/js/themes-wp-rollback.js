f63407e65e44e7b4ea6b32f71c3dccb83a625c67
devin <devin@wordimpress.com>
2015-05-08 15:41:03 -0700

    Backbone woes

--- assets/js/themes-wp-rollback.js
+++ assets/js/themes-wp-rollback.js
@@ -12,57 +12,44 @@ jQuery.noConflict();
 
 	//On DOM Ready
 	$( function () {
-		var themes;
-		themes = wp.themes = wp.themes || {};
-		themes.data = _wpThemeSettings;
-
-		//Use backbone
 		//var themes;
-		//themes.view.Theme = themes.view.Theme.extend( {
+		//themes = wp.themes = wp.themes || {};
+		//themes.data = _wpThemeSettings;
+
+
+		//var rollback = wp.Backbone.View.extend( {
 		//
-		//	render: function () {
-		//		console.log( "2 " );
-		//		this.on( "theme:expand", function ( data ) {
-		//			console.log( "member submit event " );
-		//
-		//		} );
-		//	}
 		//
 		//} );
 
-		var rollback = wp.Backbone.View.extend( {
 
-			events     : {
-				'click': 'addRollback'
-			},
-			addRollback: function ( e ) {
-				alert( 'here' );
-			}
-			//var view = new themes.view.Themes();
-			//view.on( 'theme:expand', function ( theView ) {
-			//	console.log( 'here' );
-			//	console.log( theView );
-			//} );
+		//var rollback = wp.Backbone.View.extend({
+		//			// assign a compiled template function.
+		//			template: wp.template
+		//		});
+		//var rollback = new themes.view.Theme();
+		//console.log(rollback);
+		//console.log(rollback.views.view);
 
-		} );
-
-		var themes = new themes.Model;
-
-		//var rollback = _.extend({}, Backbone.Events);
-		console.log( themes );
-
-		//var HeaderView = themes.view.Details.extend({
-		//  initialize: function() {
-		//	  themes.view.Details.prototype.initialize.call(this);
-		//
-		//	  alert('heyo');
-		//
+		//var rollback = themes.view.Theme.extend({
+		//  render: function(e){
+		//    alert("I was clicked!");
 		//  }
 		//});
 
+		console.log(wp.themes);
+		wp.themes.on('click', function(){
+			alert("here");
+		});
+
+		//console.log(rollback);
+		//rollback.$el.on('click', function(e){
+		//	console.log('hello');
+		//	alert('here');
+		//});
 
 		//console.log( rollback );
-		//$( themes ).on( 'click', function ( e ) {
+		//$( themes.template ).on( 'click', function ( e ) {
 		//	console.log( themes.template );
 		//	console.log( themes.focusedTheme );
 		//	console.log(e);
