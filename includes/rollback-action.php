8c433816efcd41199ae6cd066943e9c0bc02eae2
Devin Walker <sdtucson@gmail.com>
2020-07-21 13:42:27 -0700

    fix: provide the title variable to prevent PHP notices when rolling back

--- includes/rollback-action.php
+++ includes/rollback-action.php
@@ -11,7 +11,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Theme rollback.
 if ( ! empty( $_GET['theme_file'] ) && file_exists( WP_CONTENT_DIR . '/themes/' . $_GET['theme_file'] ) ) {
 
+
 	// Theme specific vars.
+	$title   = $_GET['rollback_name'];
 	$nonce   = 'upgrade-theme_' . $_GET['theme_file'];
 	$url     = 'index.php?page=wp-rollback&theme_file=' . $args['theme_file'] . 'action=upgrade-theme';
 	$version = $_GET['theme_version'];
@@ -29,6 +31,7 @@ if ( ! empty( $_GET['theme_file'] ) && file_exists( WP_CONTENT_DIR . '/themes/' 
 } elseif ( ! empty( $_GET['plugin_file'] ) && file_exists( WP_PLUGIN_DIR . '/' . $_GET['plugin_file'] ) ) {
 
 	// This is a plugin rollback.
+	$title   = $_GET['rollback_name'];
 	$nonce   = 'upgrade-plugin_' . $this->plugin_slug;
 	$url     = 'index.php?page=wp-rollback&plugin_file=' . esc_url( $args['plugin_file'] ) . 'action=upgrade-plugin';
 	$plugin  = $this->plugin_slug;
